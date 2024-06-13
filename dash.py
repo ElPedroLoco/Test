d0d7c2c28206577f0ecc9c8d8aab2905e6791eb8
ElPedroLoco <41363567+ElPedroLoco@users.noreply.github.com>
2024-06-13 14:48:04 +0200

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,18 +1,6 @@
 import streamlit as st
 import requests
 
-st.title('Hello, Streamlit!')
-st.write('This is a Streamlit app running alongside a Flask app.')
-
-url = 'http://ec2-35-181-155-27.eu-west-3.compute.amazonaws.com:5000/predict_client'  # Replace with your Flask server's public DNS and port
-data = {
-    "key1": "value1",
-    "key2": "value2"
-}
-
-if st.button('Send Request'):
-    response = requests.post(url, json=data)
-    if response.status_code == 200:
-        st.write(response.json())
-    else:
-        st.write(f"Error: {response.status_code}")
+response = requests.get('http://ec2-35-181-155-27.eu-west-3.compute.amazonaws.com:5000/data')
+data = response.json()
+st.write(f"Data from Flask: {data['value']}")
